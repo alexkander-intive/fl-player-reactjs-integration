333128be973cbee0c5f87dd6b69d3b3a206120cd
Diego Vasquez <dievacri@gmail.com>
2022-09-21 11:28:08 -0500

    Fix build error with fl-foundation

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,3 @@
-const path = require("path");
-
 module.exports = {
   // The Webpack config to use when compiling your react app for development or production.
   webpack: function(config, env) {
@@ -10,9 +8,9 @@ module.exports = {
         ...config.resolve,
         fallback: {
           // But in mi case I paste
-          buffer: require.resolve("buffer"),
-          crypto: require.resolve("crypto-browserify"),
-          stream: require.resolve("stream-browserify"),
+          // buffer: require.resolve("buffer"),
+          crypto: "crypto-browserify",
+          stream: "stream-browserify",
           // 'fl-player': require.resolve('./src/third-party/fl-player-es6/fl-player.es.js'),
           // 'fl-player-interface': require.resolve('./src/third-party/fl-player-es6/fl-player-interface.es.js'),
           // 'fl-foundation': require.resolve('./src/third-party/fl-player-es6/fl-foundation.es.js'),
